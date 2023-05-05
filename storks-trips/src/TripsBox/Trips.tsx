4a8b42609d75f82b1c3c5ce530326c9bf0e73f99
Piotr K <piotr.keska67@gmail.com>
2023-05-05 16:31:31 +0200

    Create TripsSelector Component

--- storks-trips/src/TripsBox/Trips.tsx
+++ storks-trips/src/TripsBox/Trips.tsx
@@ -1,41 +1,10 @@
-import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
-import { BsChevronDown } from "react-icons/bs";
+import TripsSelector from "./TripsSelector";
 
 const Trips = () => {
   return (
     <>
       <div>Trips</div>
-      <Menu>
-        <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-          Country
-        </MenuButton>
-        <MenuList>
-          <MenuItem>PL</MenuItem>
-          <MenuItem>DE</MenuItem>
-        </MenuList>
-      </Menu>
-      <Menu>
-        <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-          Members
-        </MenuButton>
-        <MenuList>
-          <MenuItem>1</MenuItem>
-          <MenuItem>2</MenuItem>
-          <MenuItem>3</MenuItem>
-          <MenuItem>4</MenuItem>
-        </MenuList>
-      </Menu>
-      <Menu>
-        <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-          Price Scope
-        </MenuButton>
-        <MenuList>
-          <MenuItem>0-100</MenuItem>
-          <MenuItem>100-200</MenuItem>
-          <MenuItem>200-300</MenuItem>
-          <MenuItem>300-400</MenuItem>
-        </MenuList>
-      </Menu>
+      <TripsSelector />
     </>
   );
 };
