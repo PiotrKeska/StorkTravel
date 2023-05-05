942e59b67550009a6d376dc743a62d4570303f1a
Piotr K <piotr.keska67@gmail.com>
2023-05-05 18:25:59 +0200

    Set Store in Zustand for TripsSelector

--- storks-trips/src/TripsBox/TripsSelector.tsx
+++ storks-trips/src/TripsBox/TripsSelector.tsx
@@ -1,16 +1,46 @@
 import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
+import useTripsSelector from "./store";
 
 const TripsSelector = () => {
+  const CountrySelector = [{ label: "PL" }, { label: "DE" }];
+
+  const MemberSelector = [
+    { value: 1 },
+    { value: 2 },
+    { value: 3 },
+    { value: 4 },
+  ];
+
+  const ScopeSelector = [
+    { value: "0-100" },
+    { value: "100-200" },
+    { value: "200-300" },
+    { value: "300-400" },
+  ];
+
+  const selectedCountry = useTripsSelector((s) => s.tripFilters.country);
+  const setSelectedCountry = useTripsSelector((s) => s.setCountry);
+  const currentCountry = CountrySelector.find(
+    (country) => country.label === selectedCountry
+  );
+
   return (
     <>
       <Menu>
         <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-          Country
+          {currentCountry?.label || "Country"}
         </MenuButton>
         <MenuList>
-          <MenuItem>PL</MenuItem>
-          <MenuItem>DE</MenuItem>
+          {CountrySelector.map((country) => (
+            <MenuItem
+              key={country.label}
+              value={country.label}
+              onClick={() => setSelectedCountry(country.label)}
+            >
+              {country.label}
+            </MenuItem>
+          ))}
         </MenuList>
       </Menu>
       <Menu>
@@ -18,10 +48,11 @@ const TripsSelector = () => {
           Members
         </MenuButton>
         <MenuList>
-          <MenuItem>1</MenuItem>
-          <MenuItem>2</MenuItem>
-          <MenuItem>3</MenuItem>
-          <MenuItem>4</MenuItem>
+          {MemberSelector.map((member) => (
+            <MenuItem key={member.value} value={member.value}>
+              {member.value}
+            </MenuItem>
+          ))}
         </MenuList>
       </Menu>
       <Menu>
@@ -29,10 +60,11 @@ const TripsSelector = () => {
           Price Scope
         </MenuButton>
         <MenuList>
-          <MenuItem>0-100</MenuItem>
-          <MenuItem>100-200</MenuItem>
-          <MenuItem>200-300</MenuItem>
-          <MenuItem>300-400</MenuItem>
+          {ScopeSelector.map((scope) => (
+            <MenuItem key={scope.value} value={scope.value}>
+              {scope.value}
+            </MenuItem>
+          ))}
         </MenuList>
       </Menu>
     </>
---
+++ storks-trips/src/TripsBox/store.ts
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+
+
+
+interface TripFilters {
+    country?: string;
+    members?: number;
+    scope?: string;
+}
+
+interface TripsFilterStore {
+    tripFilters: TripFilters,
+    setCountry: (country: string) => void;
+    setMembers: (members: number) => void;
+    setScope: (scope: string) => void;
+}
+
+
+const useTripsSelector = create<TripsFilterStore>(set => ({
+    tripFilters: {},
+    setCountry: (country) => set(store => ({tripFilters: {...store.tripFilters, country}})),
+    setMembers: (members) => set(store =>({tripFilters: {...store.tripFilters, members}})),
+    setScope: (scope) => set(store => ({tripFilters: {...store.tripFilters, scope}}))
+}))
+
+export default useTripsSelector;
