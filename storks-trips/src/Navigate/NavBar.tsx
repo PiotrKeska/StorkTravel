ca82ead550f4e736df3ffbe571097206afa393cc
Piotr K <piotr.keska67@gmail.com>
2023-05-05 16:06:30 +0200

    Setting skeleton for NavBar

--- storks-trips/src/Navigate/NavBar.tsx
+++ storks-trips/src/Navigate/NavBar.tsx
@@ -1,5 +1,12 @@
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <>
+      <a href="/home">Home </a>
+      <a href="/trips">Trips Selector </a>
+      <a href="/opinions">Opinions </a>
+      <a href="/contact">Contact</a>
+    </>
+  );
 };
 
 export default NavBar;
