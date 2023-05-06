427dab546a36a801b0fac2d257c27159e54fc7f1
Piotr K <piotr.keska67@gmail.com>
2023-05-06 15:17:46 +0200

    Refactor useCountry hook

--- storks-trips/src/TripsBox/Filters/useCountry.ts
+++ storks-trips/src/TripsBox/Filters/useCountry.ts
@@ -6,8 +6,9 @@ import useTrips from "../useTrips"
 
 const useCountry = () => {
     const {data} = useTrips();
-    const countrys = data?.results.map((el) => el.Country);
-    const setCountrys = [... new Set(countrys)];
+    const countries = data?.results.map((el) => el.Country);
+    const countriesToResponse = countries?.concat('All').sort();
+    const setCountrys = [... new Set(countriesToResponse)];
 
     return setCountrys;
 }
