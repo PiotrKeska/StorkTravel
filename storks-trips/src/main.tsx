d12d41f00420b19d55a11c3a80099e40c678707b
Piotr K <piotr.keska67@gmail.com>
2023-05-07 16:26:28 +0200

    Setting first route

--- storks-trips/src/main.tsx
+++ storks-trips/src/main.tsx
@@ -4,6 +4,8 @@ import App from "./App.tsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
+import router from "./routing/routes.tsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,6 +19,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}></RouterProvider>
         <App />
       </QueryClientProvider>
     </ChakraProvider>
